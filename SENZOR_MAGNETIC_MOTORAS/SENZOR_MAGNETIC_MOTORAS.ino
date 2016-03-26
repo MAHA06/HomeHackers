4375772c81e3adcbb5bf9c6edc7c236ee6ad3170
MAHA06 <mihai_mahalean@yahoo.com>
2016-03-26 17:18:51 +0200

    +SENZOR IR

---
+++ SENZOR_MAGNETIC_MOTORAS/SENZOR_MAGNETIC_MOTORAS.ino
@@ -0,0 +1,33 @@
+#include "IRremote.h"
+
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+    digitalWrite(7,HIGH);
+   Serial.print("ARDUINO HAS STARTED");
+   
+}
+
+void loop() {
+if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+ 
+}
+
+
+
+
+
+
+
+
